877f6bbf642ff80bc569f71be9c3ea501cc03b2c
shokkergroup <rwhittenburgtn@gmail.com>
2026-08-09 14:02:56 -0400

    Add full Batman Forever commentary dossier

--- public/demo/source-dossier-assets.js
+++ public/demo/source-dossier-assets.js
@@ -563,6 +563,7 @@
     "episode-editorial-packs-wave539.js?v=1.0.0-oct-04-2020-freddys-revenge-commentary-full-audio-human-read",
     "episode-editorial-packs-wave540.js?v=1.0.0-oct-03-2020-nightmare-1-commentary-full-audio-human-read",
     "episode-editorial-packs-wave541.js?v=1.0.0-aug-27-2020-halloween-2018-encore-full-audio-human-read",
+    "episode-editorial-packs-wave542.js?v=1.0.0-aug-10-2020-batman-forever-commentary-full-audio-human-read",
     "wwam-fam-index.js?v=1.0.0-caption-ledger",
     "episode-recap-engine.js?v=1.9.9-title-topic-carrythrough",
     "wwam-episode-recap-adapter.js?v=2.5.1-editorial-body-truth",
